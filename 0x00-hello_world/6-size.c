e5943e8cc7bf95ec4bdeb800efb49cfa5617f540
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-08 07:50:04 -0700

    Fixed specifiers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long long_long_i;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long int: %d bytes\n", sizeof(long_i));
-	printf("Size of a long long int: %d bytes\n", sizeof(long_long_i));
-	printf("Size of a float: %d byte(s)\n", sizeof(f));
-	return(0)
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld bytes\n", sizeof(long_i));
+	printf("Size of a long long int: %ld bytes\n", sizeof(long_long_i));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return(0);
 }
