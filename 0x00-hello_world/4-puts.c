1fcbb01e9e75753e478c540f6d32e0fa1f5533bf
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-08 06:25:08 -0700

    Using betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-int main(){
+//main function prints a string to stdout using the 'puts' funtion//
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
