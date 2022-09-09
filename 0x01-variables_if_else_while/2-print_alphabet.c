9ba255d44a3bd6e9f37b8d113669fb113b5ef025
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-09 11:44:33 -0700

    Program prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Program prints the alphabet in lowercase
+ *
+ * Return:0
+*/
+int main(void)
+{
+	char _alphabet;
+	for (_alphabet = 'a' ; ch <= 'z')
+	{
+		_alphabet++ ;
+	putchar(_alphabet);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Program that prints all single digit numbers of base 10 starting from 0
+ *
+ * return:0
+ *
+*/
+int main(void)
+{
+
+}
