209e29157a2d1689a6eea31ce46ce5aa5faf3319
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-13 05:10:23 -0700

    Attempt at first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main-starts the program
+ *
+ * program that prints _putchar, followed by a new line.
+ *
+ * Return:0
+*/
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project helps us look into functions and nested loops in C
