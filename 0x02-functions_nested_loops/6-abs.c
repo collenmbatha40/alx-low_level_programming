7d0f5f97245eb081f2d34406250a34f350809a18
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-13 12:15:06 -0700

    Function that computes the value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * 
+ *_abs - computes the absolut value of an integer
+ *
+ * Return:0
+ *
+*/
+int _abs(int c)
+{
+	if(c == 0 || c > 0)
+	{
+		return (c);
+	}
+	else
+		return (c * -1);
+}
