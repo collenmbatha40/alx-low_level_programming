fd32cbecfba33b5cb920facbc00b963dabaea022
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-13 07:11:26 -0700

    New attempt at task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 /**
- * main-starts the program
+ * main - starting point
  *
  * program that prints _putchar, followed by a new line.
  *
  * Return:0
-*/
+ */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar('_putchar\n');
 	return (0);
 }
