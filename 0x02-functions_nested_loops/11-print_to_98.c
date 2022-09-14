15ec26fca6718d9099d2289913d93fd09e0cdb0a
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-14 11:07:57 -0700

    Stdout error correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,7 +17,7 @@ void print_to_98(int n)
 		}
 		printf("%d\n", n);
 	}
-	else if
+	else
 	{
 		while(n >= 98)
 		{
@@ -25,8 +25,5 @@ void print_to_98(int n)
 		}
 		printf("%d\n", n);
 	}
-	else
-	{
-		printf("%d\n", n);
-	}
+	
 }
