6abdf25d9eb4305423c09f59de5f2c579e9468b7
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-15 09:33:50 -0700

    0. Multiple mains

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This project looks into debugging in the C programming language
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#ifdef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void positive_or_negative(int i)
+
+#endif /*AIN_H */
