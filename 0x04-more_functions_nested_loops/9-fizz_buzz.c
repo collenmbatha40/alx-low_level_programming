9eeb257c6ad888518f48d321b89dc0771e789682
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-16 13:02:32 -0700

    Reattempt

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fizz_buzz - printing to 100
+ * Return: 0
+ */
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+}
