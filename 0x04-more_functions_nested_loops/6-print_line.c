9365d7fe73621e8c32131e6507273aada29117c9
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-16 11:32:24 -0700

    Task 6 attempt

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints a straight line
+ * @n - function parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	while (n -- > 0)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
