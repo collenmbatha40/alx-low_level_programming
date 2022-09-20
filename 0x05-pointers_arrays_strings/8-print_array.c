91879fe58a100f08050afe4b083e76ab88499c9a
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-20 03:58:02 -0700

    Reattempt task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,18 +7,18 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; i < n; i++)
-	{
-		if (i != n - 1)
-		{
-			printf("%d, ", a[i]);
+for (i = 0; i < n; i++)
+{
+if (i != n - 1)
+{
+printf("%d, ", a[i]);
+}
+else
+{
+printf("%d", a[i]);
 		}
-		else
-		{
-			printf("%d", a[i]);
-		}
-	}
-	_putchar(10);
+}
+_putchar(10);
 }
