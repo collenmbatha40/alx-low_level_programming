f032cad493f4cb88359b38f891dc1bdc96cb199e
Anele Mbatha <collenmbatha40@gmail.com>
2022-09-28 08:38:39 -0700

    function that checks if a string is a palindrome using recursion

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * palindrome - checks if string is a palindrome
+ * @s: string being checked
+ * @index1: starting index
+ * @index2: ending index
+ * @pldrm: shows whether s is a palindrome
+ * Return: void
+ */
+void palindrome(char *s, int index1, int index2, int pldrm)
+{
+	if (index1 <= index2)
+	{
+		if (s[index1] == s[index2])
+		{
+			*pldrm *= 1;
+		}
+		else
+		{
+			*pldrm *= 0;
+		}
+		palindrome(s, index1 + 1, index2 - 1, pldrm);
+	}
+}
+
+/**
+ * is_palindrome -  returns 1 if a string is a palindrome
+ * @s: string being checked
+ * Return: if s is a palindrome return 1, else 0
+ */
+int is_palindrome(char *s)
+{
+	int pldrm = 1;
+
+	palindrome(s, 0, _strlen_recursion(s) - 1, &pldrm);
+	return (pldrm);
+}
