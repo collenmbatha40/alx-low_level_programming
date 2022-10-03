c15522f7e999245373a68c75147255d6b9301bb5
Anele Mbatha <collenmbatha40@gmail.com>
2022-10-03 07:20:03 -0700

    Task 3 attempt

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - prints the result of the multiplication of 2 numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, mult;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mult = num1 * num2;
+		printf("%d\n", mult);
+	}
+	return (0);
+}
