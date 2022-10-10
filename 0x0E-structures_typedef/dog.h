3be9772cfa4cfcda6859df7f19b84ef3772f6bde
Anele Mbatha <collenmbatha40@gmail.com>
2022-10-10 07:13:09 -0700

    Defined a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure named dog
+ * @name: char pointer to dog name
+ * @age: float defining dog's age
+ * @owner: char pointer to dog owner's name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /*DOG_H*/
