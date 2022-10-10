e63cb1e5ad4367a8f26a1a6fc077146214583073
Anele Mbatha <collenmbatha40@gmail.com>
2022-10-10 07:30:03 -0700

    Quick fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,10 +9,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (d == 0)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		return;
 	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
