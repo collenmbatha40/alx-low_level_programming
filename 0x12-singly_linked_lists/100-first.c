d581ba6c962b89d46a11db24139d8ebb6af97483
Anele Mbatha <collenmbatha40@gmail.com>
2022-10-20 03:54:33 -0700

    Advanced task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+ * hare - function executed before main
+ */
+void __attribute__((constructor)) hare(void)
+
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
