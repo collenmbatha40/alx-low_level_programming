216d6cefa87ab9c7d0af7364a38cf9b1d1db6567
Anele Mbatha <collenmbatha40@gmail.com>
2022-10-25 00:11:18 -0700

    function that frees a list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
-	
+
 	new = malloc(sizeof(listint_t));
 	if (!new)
 	{
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	new->n = n;
 	new->next = NULL;
-	
+
 	if (*head == NULL)
 	{
 		*head = new;
@@ -28,6 +28,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		temp = temp->next;
 	}
 	temp->next = new;
-	
+
 	return (new);
 }
