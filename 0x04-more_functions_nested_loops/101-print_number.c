6481cf48b2304f47cd303ef56321429a6214ac0b
Anele Mbatha <collenmbatha40@gmail.com>
2022-11-10 07:55:56 -0800

    Advanced tasks

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,29 +5,18 @@
  */
 void print_number(int n)
 {
-	unsigned int i, j, count;
+unsigned int m;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		i = n * -1;
-	}
-	else
-	{
-		i = n;
-	}
-
-	j = i;
-	count = 1;
-
-	while (j > 9)
-	{
-		j /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((i / count) % 10) + 48);
-	}
+if (n < 0)
+{
+_putchar('-');
+m = -n;
+}
+else
+{
+m = n;
+}
+if (m / 10 != 0)
+print_number(m / 10);
+_putchar((m % 10) + '0');
 }
