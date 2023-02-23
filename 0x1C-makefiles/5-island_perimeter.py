9beb78bb989510c83ea67b91a635da695524b96a
Anele Mbatha <collenmbatha40@gmail.com>
2023-02-22 23:05:44 -0800

    Technical interview prep question

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,20 +9,18 @@ def island_perimeter(grid):
     This is a function that returns the perimeter
     of the island described in grid.
     """
-    if len(grid) == 0 or not isinstance(grid, list):
-        return 0
-    else:
-        for a in grid:
-            if len(a) == 0:
-                return 0
-            elif not isinstance(a, list):
-                return 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    n = 0
-    for unit in grid:
-        for cell in unit:
-            if cell == 1:
-                n += 2
-    if n:
-        n += 2
-    return n
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
+
